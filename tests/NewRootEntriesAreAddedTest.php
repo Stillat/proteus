d1585b0ffb58c89e0b78b4f561815f5d4391b811
John Koster <john@stillat.com>
2020-10-24 06:29:39 -0500

    Adds test to ensure that expected nested forms is added properly as new entry

--- tests/NewRootEntriesAreAddedTest.php
+++ tests/NewRootEntriesAreAddedTest.php
@@ -5,7 +5,9 @@ use Stillat\Proteus\ConfigUpdater;
 use Stillat\Proteus\Document\Transformer;
 
 if (!function_exists('env')) {
-    function env($key, $default= null) {}
+    function env($key, $default = null)
+    {
+    }
 }
 
 class NewRootEntriesAreAddedTest extends TestCase
@@ -14,7 +16,7 @@ class NewRootEntriesAreAddedTest extends TestCase
     public function testThatNewRootArrayEntriesAreAdded()
     {
         $updater = new ConfigUpdater();
-        $updater->open(__DIR__.'/configs/app.php');
+        $updater->open(__DIR__ . '/configs/app.php');
         $expected = Transformer::normalizeLineEndings(file_get_contents(__DIR__ . '/expected/simplenewkey.php'));
         $updater->update([
             'new' => [
@@ -31,7 +33,7 @@ class NewRootEntriesAreAddedTest extends TestCase
     public function testThatNewKeysUsingDotNotationAreProperlyNested()
     {
         $updater = new ConfigUpdater();
-        $updater->open(__DIR__.'/configs/app.php');
+        $updater->open(__DIR__ . '/configs/app.php');
         $expected = Transformer::normalizeLineEndings(file_get_contents(__DIR__ . '/expected/simpledotnotationkeyset.php'));
         $updater->update([
             'new.key' => 'value'
@@ -45,7 +47,7 @@ class NewRootEntriesAreAddedTest extends TestCase
     public function testThatNewDeeplyNestedKeysAreCreated()
     {
         $updater = new ConfigUpdater();
-        $updater->open(__DIR__.'/configs/app.php');
+        $updater->open(__DIR__ . '/configs/app.php');
         $expected = Transformer::normalizeLineEndings(file_get_contents(__DIR__ . '/expected/deeplynestedtest.php'));
         $updater->update([
             'new.deeply.nested.key' => [
@@ -62,7 +64,7 @@ class NewRootEntriesAreAddedTest extends TestCase
     public function testThatNewSimpleItemsAreAppendedToArrays()
     {
         $updater = new ConfigUpdater();
-        $updater->open(__DIR__.'/configs/appendarray.php');
+        $updater->open(__DIR__ . '/configs/appendarray.php');
         $expected = Transformer::normalizeLineEndings(file_get_contents(__DIR__ . '/expected/appendarray.php'));
 
         // Using the assignment method should just append to the existing values.
@@ -78,12 +80,47 @@ class NewRootEntriesAreAddedTest extends TestCase
     public function testThatResettingAnArrayReplacesTheArray()
     {
         $updater = new ConfigUpdater();
-        $updater->open(__DIR__.'/configs/appendarray.php');
+        $updater->open(__DIR__ . '/configs/appendarray.php');
         $expected = Transformer::normalizeLineEndings(file_get_contents(__DIR__ . '/expected/arrayreplacesarray.php'));
 
         // Using the assignment method with an array value should replace an existing array.
         $updater->update([
             'test' => ['new-value']
+        ]);
+
+        $doc = $updater->getDocument();
+
+        $this->assertEquals($expected, $doc);
+    }
+
+    public function testThatNewNestedEntriesAreProperlyCreated()
+    {
+
+        $updater = new ConfigUpdater();
+        $updater->open(__DIR__ . '/configs/issues/001/setnew.php');
+        $expected = Transformer::normalizeLineEndings(file_get_contents(__DIR__ . '/expected/issues/001/setnew.php'));
+
+        // Using the assignment method with an array value should replace an existing array.
+        $updater->update([
+            'forms' => [
+                'contact_us' => [
+                    'name_field' => 'name',
+                    'first_name_field' => 'first_name',
+                    'last_name_field' => 'last_name',
+                    'email_field' => 'email',
+                    'content_field' => 'message',
+                    'handle' => 'contact_us',
+                ],
+                'contact_you' =>
+                    [
+                        'name_field' => 'name3',
+                        'first_name_field' => 'first_name',
+                        'last_name_field' => 'last_name',
+                        'email_field' => 'email3',
+                        'content_field' => 'message',
+                        'handle' => 'contact_you',
+                    ],
+            ]
         ]);
 
         $doc = $updater->getDocument();
---
+++ tests/expected/issues/001/setnew.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    'api_key' => 'xxxxxxxxxxxx', 'forms' => [
+        'contact_us' => [
+            'name_field' => 'name',
+            'first_name_field' => 'first_name',
+            'last_name_field' => 'last_name',
+            'email_field' => 'email',
+            'content_field' => 'message',
+            'handle' => 'contact_us',
+        ],
+        'contact_you' => [
+            'name_field' => 'name3',
+            'first_name_field' => 'first_name',
+            'last_name_field' => 'last_name',
+            'email_field' => 'email3',
+            'content_field' => 'message',
+            'handle' => 'contact_you',
+        ],
+    ],
+
+];
