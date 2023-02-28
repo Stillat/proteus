d2016601a6ecdfb5c165ace865b267c240edd649
John Koster <john@stillat.com>
2023-02-28 08:02:28 -0600

    Add coverage for adding a new dynamic node

--- tests/WriteTest.php
+++ tests/WriteTest.php
@@ -157,4 +157,62 @@ EXP;
 
         $this->assertSame($expected, $updater->getDocument());
     }
+
+    public function testAutomaticInsertionInWriteMode()
+    {
+        $updater = new ConfigUpdater();
+        $updater->open(__DIR__.'/configs/issues/018.php');
+
+        $updater->update([
+            'tasks' => [
+                'Task 1',
+                'Task 2',
+            ],
+        ], false);
+
+        $expected = <<<'EOT'
+<?php
+
+return [
+    'forms' => [
+        [
+            'id' => 'id1',
+            'form' => 'contact',
+            'check_consent' => true,
+            'consent_field' => 'newsletter',
+            'disable_opt_in' => false,
+            'marketing_permissions_field_ids' => [
+                [
+                ],
+            ],
+            'merge_fields' => [
+                [
+                    'id' => 'id2',
+                    'field_name' => 'first_name',
+                    'tag' => 'FNAME',
+                ],
+                [
+                    'id' => 'id3',
+                    'field_name' => 'last_name',
+                    'tag' => 'LNAME',
+                ],
+                [
+                    'id' => 'id',
+                    'field_name' => 'company',
+                    'tag' => 'COMPANY',
+                ],
+            ],
+            'primary_email_field' => 'email',
+            'audience_id' => 'aud1',
+        ],
+    ], 'tasks' => [
+        'Task 1',
+        'Task 2',
+    ],
+];
+
+EOT;
+
+        $this->assertSame($expected, $updater->getDocument());
+    }
 }
